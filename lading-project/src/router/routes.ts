37a4f472f7f5f5c1e00ef7767d1bc93c3cba7db6
kimkyutack <85102749+metacare-dev@users.noreply.github.com>
2024-03-12 09:03:37 +0900

    Delete: comment

--- lading-project/src/router/routes.ts
+++ lading-project/src/router/routes.ts
@@ -5,9 +5,6 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('pages/IndexPage.vue'),
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
