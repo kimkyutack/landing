44dd56a2f0374b53a3a401b41e512e2a450d962f
kimkyutack <85102749+metacare-dev@users.noreply.github.com>
2024-03-13 10:02:33 +0900

    Add: notify in func

--- lading-project/src/components/Input.vue
+++ lading-project/src/components/Input.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { axiosStore } from '@/stores/axios';
+import { useQuasar } from 'quasar';
 import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const axios = axiosStore();
+const $q = useQuasar();
 const route = useRoute();
 
 const id = computed(() => route.params?.id);
@@ -33,20 +35,36 @@ const inputClass =
   'flex-2 border-none py-1 text-2xl border-round-lg text-center';
 
 function autoHyphen(event: any) {
-  inputValue.value[2].value = event.target.value
+  inputValue.value[2].value = event
     .replace(/[^0-9]/g, '') // 숫자를 제외한 모든 문자 제거
     .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
 }
 
 async function returnValue() {
+  if (inputValue.value.filter((item: any) => item.value === '')?.length !== 0) {
+    $q.notify({
+      message: '모든 정보를 입력해주세요.',
+      type: 'negative',
+    });
+    return;
+  }
   const result = await axios.request('post', '/landing/applicant_save.php', {
     landing_key: id.value,
     tel: inputValue.value[0].value,
-    age: inputValue.value[1].value,
+    age: parseInt(inputValue.value[1].value, 10),
     name: inputValue.value[2].value,
   });
-  if (result) console.log('성공');
-  else console.log('실패');
+  if (result?.success) {
+    $q.notify({
+      message: 'success',
+      type: 'positive',
+    });
+  } else {
+    $q.notify({
+      message: 'fail',
+      type: 'negative',
+    });
+  }
 }
 </script>
 
@@ -66,8 +84,8 @@ async function returnValue() {
         :id="item.key"
         :placeholder="item.placeholder"
         v-model="item.value"
-        @input="(event: any) => (item.name === '전화번호' ? autoHyphen(event) : '')"
-        :maxlength="item.name === '전화번호' ? 13 : 20"
+        @update:model-value="(event: any) => (item.key === 'tel' ? autoHyphen(event) : '')"
+        :maxlength="item.key === 'tel' ? 13 : 20"
       />
     </article>
     <q-btn
